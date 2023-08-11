1fe44c006a67d7b5e9b1025cd563b9e2b7d55578
Rahul Dewatwal <114424031+RahulDewatwal@users.noreply.github.com>
2023-08-11 22:01:16 +0530

    Update and rename Program_that_reads_a_floating-point_number_and_then_displays_the_right-most_digit_of_the_integral_part_of_the_number.c to Program_to_add_two_numbers_in_java.java

---
+++ Program_to_add_two_numbers_in_java.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Add{
+public static void main(String [] args){
+int num1,num2;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter two Number:");
+num1=sc.nextInt();
+num2=sc.nextInt();
+int sum=num1+num2;
+System.out.println("sum="+sum);
+}
+}
